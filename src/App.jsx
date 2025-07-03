2df3faa479ce834cde366b9479fa1cfd0b8b2699
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-07-03 22:38:46 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import './App.css'
+import "./App.css";
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
@@ -8,25 +8,23 @@ import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Footer from "./components/Footer/Footer";
 
-
-
 function App() {
-	return (
-		<div className="App">
-			<div>
-				<div className="white-gradient"></div>
-			<Header></Header>
-			<Hero/>
-			</div>
+    return (
+        <div className="App">
+            <div>
+                <div className="white-gradient"></div>
+                <Header></Header>
+                <Hero />
+            </div>
 
-			<Companies/>
-			<Residencies/>
-			<Value/>
-			<Contact/>
-			<GetStarted/>
-			<Footer/>
-		</div>
-	);
+            <Companies />
+            <Residencies />
+            <Value />
+            <Contact />
+            <GetStarted />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
