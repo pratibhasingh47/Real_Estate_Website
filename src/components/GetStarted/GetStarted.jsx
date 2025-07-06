395a7ad0090981625902ab3bffa223ebe4c864fd
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-07-07 00:18:01 +0530

    components

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import './GetStarted.css'
+import React from "react";
+import "./GetStarted.css";
 
 const GetStarted = () => {
-	return (
-		<section className="g-wrapper">
-			<div className="paddings innerWidth g-container">
-				<div className="flexColCenter inner-container">
-					<span className='primaryText'>Get Started with Homyz</span>
-					<span className='secondaryText'>Subscribe and find super attractive price quotes from us.<br />
-						Find your residnce soon</span>
-					<button className='button'>
-						<a href="#">Get Started</a>
-					</button>
-				</div>
-			</div>
-		</section>
-	)
-}
+    return (
+        <section className="g-wrapper">
+            <div className="paddings innerWidth g-container">
+                <div className="flexColCenter inner-container">
+                    <span className="primaryText">Get Started with Homyz</span>
+                    <span className="secondaryText">
+                        Subscribe and find super attractive price quotes from
+                        us.
+                        <br />
+                        Find your residnce soon
+                    </span>
+                    <button className="button">
+                        <a href="#">Get Started</a>
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default GetStarted
+export default GetStarted;
